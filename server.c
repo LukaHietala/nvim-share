4959b5bcd039160162042501260ffd259aa119cd
Luka Hietala <luka.hietala08@gmail.com>
2026-01-03 13:15:08 +0200

    add connetion messaging and refactor

--- server.c
+++ server.c
@@ -8,30 +8,34 @@
 #include <string.h>
 
 #define MAX_CLIENTS 64
-
-struct client {
-	int is_host;
-};
+#define BUF_SIZE    8192
 
 struct server {
 	struct pollfd fds[MAX_CLIENTS];
-	struct client clients[MAX_CLIENTS];
 	int nfds;
 	int host_fd;
 };
 
-static void remove_client(struct server *srv, int idx)
+static void remove_client(struct server *srv, int client_id)
 {
-	int fd = srv->fds[idx].fd;
+	int fd = srv->fds[client_id].fd;
 	printf("Client on socket %d disconnected\n", fd);
+
+	int is_host = (fd == srv->host_fd);
+
+	if (srv->host_fd != -1 && !is_host) {
+		char msg[64];
+		int len = snprintf(msg, sizeof(msg), "0:DISCONNECT:%d\n", fd);
+		send(srv->host_fd, msg, len, 0);
+	}
+
 	close(fd);
 
-	if (srv->clients[idx].is_host)
+	if (is_host)
 		srv->host_fd = -1;
 
 	/* Move last element to current position to fill gap */
-	srv->fds[idx] = srv->fds[srv->nfds - 1];
-	srv->clients[idx] = srv->clients[srv->nfds - 1];
+	srv->fds[client_id] = srv->fds[srv->nfds - 1];
 	srv->nfds--;
 }
 
@@ -56,66 +60,74 @@ static void accept_connection(int server_fd, struct server *srv)
 	srv->fds[srv->nfds].events = POLLIN;
 
 	int is_host = (srv->host_fd < 0);
-	srv->clients[srv->nfds].is_host = is_host;
 	if (is_host)
 		srv->host_fd = new_fd;
+	else {
+		char msg[64];
+		int len = snprintf(msg, sizeof(msg), "0:CONNECT:%d\n", new_fd);
+		send(srv->host_fd, msg, len, 0);
+	}
 
 	printf("New connection: %d%s\n", new_fd, is_host ? " (host)" : "");
 	srv->nfds++;
 }
 
-/* Sends list of connected sockets to lua client */
-static void handle_query(int client_fd, struct server *srv)
+static int handle_client_data(struct server *srv, int client_id)
 {
-	char resp[1024];
-	int len = 0;
+	char buf[BUF_SIZE];
+	ssize_t n = recv(srv->fds[client_id].fd, buf, sizeof(buf) - 1, 0);
 
-	/* Skip index 0 (server socket) */
-	for (int i = 1; i < srv->nfds && len < (int)sizeof(resp); ++i) {
-		int fd = srv->fds[i].fd;
-		int is_host = srv->clients[i].is_host;
-		len += snprintf(resp + len, sizeof(resp) - len, "%d%s\n", fd,
-				is_host ? " HOST" : "");
-	}
-	send(client_fd, resp, (size_t)len, 0);
-}
-
-static int handle_client_data(struct server *srv, int idx)
-{
-	char buf[1024];
-	ssize_t n = recv(srv->fds[idx].fd, buf, sizeof(buf) - 1, 0);
-
-	/* Usually happens on disconnect */
 	if (n <= 0) {
-		remove_client(srv, idx);
+		remove_client(srv, client_id);
 		return -1;
 	}
 
 	buf[n] = '\0';
-	printf("Got %zd bytes from %d: %s\n", n, srv->fds[idx].fd, buf);
+	int client_fd = srv->fds[client_id].fd;
 
-	/* Handle commands */
-	if (strncmp(buf, "QUERY", 5) == 0)
-		handle_query(srv->fds[idx].fd, srv);
+	if (client_fd == srv->host_fd) {
+		/* Host sending to client: "TARGET_FD:DATA" */
+		char *sep = strchr(buf, ':');
+		if (sep) {
+			*sep = '\0';
+			int target_fd = atoi(buf);
+			char *data = sep + 1;
+			size_t data_len = n - (sep - buf) - 1;
+
+			for (int i = 0; i < srv->nfds; ++i) {
+				if (srv->fds[i].fd == target_fd) {
+					send(target_fd, data, data_len, 0);
+					break;
+				}
+			}
+		}
+	} else if (srv->host_fd != -1) {
+		/* Client sending to host: "DATA" -> "CLIENT_FD:DATA" */
+		char fwd_buf[BUF_SIZE + 32];
+		int prefix_len =
+			snprintf(fwd_buf, sizeof(fwd_buf), "%d:", client_fd);
+		if (prefix_len > 0 &&
+		    (size_t)prefix_len + n < sizeof(fwd_buf)) {
+			memcpy(fwd_buf + prefix_len, buf, n);
+			send(srv->host_fd, fwd_buf, prefix_len + n, 0);
+		}
+	}
+
 	return 0;
 }
 
-int main(void)
+int main()
 {
-	/* Create server socket (IPv4, stream... TCP)*/
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
 		fprintf(stderr, "Socket creation failed\n");
 		return 1;
 	}
 
-	/* Disable Nagle (to reduce delay) and make sure that the kernel
-	 * reclaims port immidietly after closing the socket */
 	int opt = 1;
 	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 	setsockopt(server_fd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt));
 
-	/* Create socket address with port 8080, and IP 0.0.0.0 */
 	struct sockaddr_in addr = { .sin_family = AF_INET,
 				    .sin_port = htons(8080),
 				    .sin_addr.s_addr = INADDR_ANY };
@@ -132,18 +144,14 @@ int main(void)
 
 	printf("Listening...\n");
 
-	/* Create server that requests POLLIN events. nfds = number of file
-	 * descriptions (thank GNU) */
 	struct server srv = { .nfds = 1, .host_fd = -1 };
 	srv.fds[0].fd = server_fd;
 	srv.fds[0].events = POLLIN;
 
-	/* Take up main thread to listen incoming POLLIN events */
 	while (poll(srv.fds, (nfds_t)srv.nfds, -1) >= 0) {
 		for (int i = 0; i < srv.nfds; ++i) {
 			if (srv.fds[i].revents &
 			    (POLLERR | POLLHUP | POLLNVAL)) {
-				/* TODO: print error to stderr */
 				remove_client(&srv, i);
 				i--;
 				continue;
